5195cc8a96a8e3b8677576564b7d85e2fcd9dd2f
adama diouf <adamadiouf2017@gmail.com>
2023-11-08 07:05:15 -0600

    chnage

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,10 +1,66 @@
 import React from 'react'
+import a from "../assets/p1.png"
+import b from '../assets/p2.png'
+import c from '../assets/p3.png'
+import d from '../assets/p4.png'
+import e from '../assets/p5.png'
+import f from '../assets/p6.png'
+import g from '../assets/p7.png'
+import h from '../assets/p8.png'
+import i from '../assets/p9.png'
+import j from '../assets/p10.png'
+import k from '../assets/p11.png'
+import l from '../assets/p12.png'
+
+
+
+
+
+
 
 const Work = () => {
   return (
     <div className='work section-center'>
       <div className='title'>
         <h3>Recently Completed Projects</h3>
+      </div>
+      <div className='work-container'>
+        <article className='work-content'>
+          <img src={a} alt='' />
+        </article>
+        <article className='work-content'>
+          <img src={b} alt='' />
+        </article>
+        <article className='work-content'>
+          <img src={c} alt='' />
+        </article>
+        <article className='work-content'>
+          <img src={d} alt='' />
+        </article>
+        <article className='work-content'>
+          <img src={e} alt='' />
+        </article>
+        <article className='work-content'>
+          <img src={f} alt='' />
+        </article>
+        <article className='work-content'>
+          <img src={g} alt='' />
+        </article>
+        <article className='work-content'>
+          <img src={h} alt='' />
+        </article>
+        <article className='work-content'>
+          <img src={i} alt='' />
+        </article>
+        <article className='work-content'>
+          <img src={j} alt='' />
+        </article>
+        <article className='work-content'>
+          <img src={k} alt='' />
+        </article>
+        <article className='work-content'>
+          <img src={l} alt='' />
+        </article>
       </div>
     </div>
   )
